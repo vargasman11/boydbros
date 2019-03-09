10f369f5d666330789f5f79037bad3948058aecf
Jerry Vargas <jvargas@conversionia.com>
2019-03-09 09:30:50 -0600

    adding more changes

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/* ========================================================================== */
+/*                 THIS IS THE CUSTOM CODE TO USE WITH STICKY                 */
+/* ========================================================================== */
+
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
@@ -8,14 +12,27 @@ var navbarMobile = document.getElementById("navbarMobile");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+// get top logo
+var logo = document.getElementById('logo');
+
+// get the jumbotron id for offset purposes
+var jumbotron = document.getElementById('jumbotronID');
+
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
+    // logo.style.display = "none";
     navbar.classList.add("sticky");
     navbarMobile.classList.add("sticky");
+    jumbotron.style.marginTop = navbar.offsetTop + 50;
+  
   } else {
+    // logo.style.display = "block";
     navbar.classList.remove("sticky");
     navbarMobile.classList.remove("sticky");
+    jumbotron.style.marginTop = "0px";
+
   }
 }
 
